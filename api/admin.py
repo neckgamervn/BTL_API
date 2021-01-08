6af476d01b44f0c60e885e35657b67dde0e83f57
Quoc Anh <quocanhnguyen199@gmail.com>
2021-01-08 23:53:48 +0700

    them get_list_post

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from api.authentication import check_existed_token
 from database.connect import read_sql_query
 from utils import on_success, on_fail
+import unicodedata
+import re
 
 
 def create_post(token):
@@ -22,3 +24,43 @@ def get_list_category():
     except Exception as err:
         print(err)
         return on_fail()
+
+def get_list_post(id_category, keyword):
+    try:
+        list_output = []
+        list_post = read_sql_query("select * from post where id_category = '{}'".format(id_category)).values
+        for post in list_post:
+            if keyword.lower() in un_unicode(post[2]).lower():
+                data = {
+                    'id': post[0],
+                    'title': post[1],
+                    'description': post[2],
+                    'url_linked': post[3],
+                    'img_url': post[4],
+                    'id_category': post[5]
+                }
+                list_output.append(data)
+        return on_success(list_output)
+    except Exception as err:
+        print("List post error: ", err)
+        return on_fail()
+
+
+def un_unicode(text):
+
+    patterns = {
+        '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+        '[đ]': 'd',
+        '[èéẻẽẹêềếểễệ]': 'e',
+        '[ìíỉĩị]': 'i',
+        '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+        '[ùúủũụưừứửữự]': 'u',
+        '[ỳýỷỹỵ]': 'y'
+    }
+
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Header
 from typing import Optional
 from api.authentication import login, register, logout, check_existed_token
 from database.models import LoginBody, RegisterBody
-from api.admin import get_list_category
+from api.admin import get_list_category, get_list_post
 from utils import on_fail
 
 app = FastAPI()
@@ -26,3 +26,7 @@ def _logout(token: Optional[str] = Header(None)):
 @app.get("/get_list_category")
 def _get_list_category(token: Optional[str] = Header(None)):
     return get_list_category() if check_existed_token(token) is not None else on_fail()
+
+@app.get("/get_list_post")
+def _get_list_post(token: Optional[str] = Header(None),id_category='0',keyword=''):
+    return get_list_post(id_category, keyword) if check_existed_token(token) is not None else on_fail()
